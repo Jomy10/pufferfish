48c0f17e6008ae44d0454fd597670919132254f9
Jomy10 <jonas.vbs4@gmail.com>
2022-02-07 19:52:26 +0100

    Fixing nodjs

--- npm/bin/pufferfish.js
+++ npm/bin/pufferfish.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 var shell = require("shelljs");
 let arguments = ""
+let scriptPath = ""
 process.argv.forEach((val, index, array) => {
-    console.log(index, val);
+    // console.log(index, val);
     if (index != 0 && index != 1) {
         arguments += val + " "
+    } else if (index == 1) {
+        scriptPath = val;
     }
 });
-shell.exec(`$(dirname "$1")/../gem/bin/puf ${arguments}`);
+shell.exec(`echo $(dirname ${scriptPath})`)
+shell.exec(`cd $(dirname ${scriptPath}) && cd ../gem/bin && ./puf ${arguments}`);
