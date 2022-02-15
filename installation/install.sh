c5bbfbd8834209a48f3a08cfc0b882bfb2c14393
Jomy10 <jonas.vbs4@gmail.com>
2022-02-15 21:01:47 +0100

    Added installation for Windows to script

--- installation/install.sh
+++ installation/install.sh
@@ -32,11 +32,9 @@ then
 
     TAR_NAME="pufferfish.tar.gz"
     TAR_FILE="$BASE_PATH$TAR_NAME"
-    echo "tar_dir: $TAR_FILE"
 
     EXTRACTED_NAME="pufferfish/"
     EXTRACTED_PATH="$BASE_PATH$EXTRACTED_NAME"
-    echo "Extracted: $EXTRACTED_PATH"
     
     mkdir $EXTRACTED_PATH
 
@@ -61,11 +59,9 @@ then
 
     TAR_NAME="pufferfish.tar.gz"
     TAR_FILE="$BASE_PATH$TAR_NAME"
-    echo "tar_dir: $TAR_FILE"
 
     EXTRACTED_NAME="pufferfish/"
     EXTRACTED_PATH="$BASE_PATH$EXTRACTED_NAME"
-    echo "Extracted: $EXTRACTED_PATH"
     
     mkdir $EXTRACTED_PATH
 
@@ -74,15 +70,34 @@ then
     tar -xf "$TAR_FILE" -C "$EXTRACTED_PATH"
 
     EXEC_NAME="puf"
-    mv "$EXTRACTED_PATH$EXEC_NAME" "/usr/local/bin/puf"
+    mv "$EXTRACTED_PATH$EXEC_NAME" "/usr/local/bin/$EXEC_NAME"
 
     echo "Cleaning up"
 
     rm -r "$BASE_PATH"
 elif [[ $OS == "windows" ]]
 then
-    # echo "\x1b[36mHi, I do not own a Windows computer. \nIf after this installation you can type \"puf\" in the terminal and get output, please let me know through either opening an issue, or opening a pull request and removing this line in the install script.\nIf it doesn't work, please open an issue with the terminal output and I'll try to fix this!\x1b[0m"
-    echo "windows is currently not supported with install script, please install manually"
+    echo "\x1b[36mHi, I do not own a Windows computer. \nIf after this installation you can type \"puf\" in the terminal and get output, please let me know through either opening an issue, or opening a pull request and removing this line in the install script.\nIf it doesn't work, please open an issue with the terminal output and I'll try to fix this!\nNOTE: you might want to try typing \"puf.exe\" in the terminal? I'm really not sure how this works on Windows.\x1b[0m"
+
+    TMPDIR="\Users\<username>\AppData\Local\Temp\\"
+    BASE_FOLDER="be.jonaseveraert.pufferfish\\"
+    BASE_PATH="$TMPDIR$BASE_FOLDER"
+    mkdir $BASE_PATH
+
+    ZIP_NAME="pufferfish.tar.gz"
+    ZIP_FILE="$BASE_PATH$TAR_NAME"
+
+    EXTRACTED_NAME="pufferfish\\"
+    EXTRACTED_PATH="$BASE_PATH$EXTRACTED_NAME"
+
+    mkdir $EXTRACTED_PATH
+
+    curl -L "$URL" --output "$ZIP_FILE"
+    echo "File downloaded to $ZIP_FILE"
+    unzip -d "$EXTRACTED_PATH" "$ZIP_FILE"
+
+    EXEC_NAME="puf.exe"
+    mv "$EXTRACTED_PATH$EXEC_NAME" "\Windows\System32\$EXEC_NAME"
 fi
 
 echo "Done."
