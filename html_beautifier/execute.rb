dc6cd71f100da4be0b7711a063733390100d25fe
Jomy10 <jonas.vbs4@gmail.com>
2022-02-15 18:01:39 +0100

    uncommented in Beautify script

--- html_beautifier/execute.rb
+++ html_beautifier/execute.rb
@@ -1,6 +1,5 @@
 # Transform an html file to ruby
 file_contents = ARGV.first
-# beautified_contents = HtmlBeautifier.beautify(file_contents)
-# puts beautified_contents
+beautified_contents = HtmlBeautifier.beautify(file_contents)
+puts beautified_contents
 puts file_contents
-puts ":)"
