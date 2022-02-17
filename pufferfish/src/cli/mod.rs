7446c1b4177bfec28e20738acb138a6fc0139869
Jomy10 <jonas.vbs4@gmail.com>
2022-02-17 12:38:51 +0100

    Code refactor

--- pufferfish/src/cli/mod.rs
+++ pufferfish/src/cli/mod.rs
@@ -1,8 +1,9 @@
 //! Contains everything for the CLI
 
-pub mod cli;
+mod cli;
 pub mod build_html;
 pub mod file_listener;
 mod clap_app;
 
 pub use clap_app::get_matches;
+pub use cli::*;
--- pufferfish/src/main.rs
+++ pufferfish/src/main.rs
@@ -1,48 +1,7 @@
-use puf::cli::cli::CLIExecutor;
+use puf::cli::CLIExecutor;
 use puf::cli;
 
 // TODO: cached; make a tree of files and their templates ('dependencies' -> remake all files depending on the changed file)
 fn main() {
-    
-    
-    // match rutie::VM::eval("puts \"hello\"") {
-    //     Ok(e) => println!("{:?}", e),
-    //     Err(e) => println!("{}", e)
-    // }
-    
     CLIExecutor::new(cli::get_matches()).execute();
-}
-
-
-struct MyTestSTruture {
-    int: u32,
-    other: String
-}
-
-fn __main() {
-    let config = PufferfishConfig::from_toml("pufferfish.toml");
-    let mut listener = puf::file_listener::FileListener::new(config);
-    let my_struct = MyTestSTruture { int: 0, other: String::new() };
-    // TODO: test with listener
-    let lock = Arc::new(RwLock::new(my_struct));
-    let lock2 = lock.clone();
-    let lock3 = lock.clone();
-    
-    let t1 = thread::spawn(move || {
-        let mut l = lock.write().unwrap();
-        l.other = String::from("Hello");
-    });
-    
-    let t2 = thread::spawn(move || {
-        let mut l = lock2.write().unwrap();
-        l.other = format!("{} world", l.other);
-    });
-    
-    
-    t1.join();
-    t2.join();
-    
-    let l = lock3.write().unwrap();
-    println!("{}", l.other);
-    
 }
